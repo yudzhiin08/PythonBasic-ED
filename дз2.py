e8314f97be3e856304c530b81983828149427bfc
yudzhiin08 <ev.ya.dddd@gmail.com>
2024-02-17 14:17:19 +0200

    Add file

--- дз2.py
+++ дз2.py
@@ -1,17 +1,17 @@
-number = int('12345')
+number = int('1234')
 print(int(number))
 
 first_number = number % 10
-print(first_number)
 number = number // 10
 second_number = number % 10
+number = number // 10
+third_number = number % 10
+number = number // 10
+fourth_number = number % 10
+print(fourth_number)
+print(third_number)
 print(second_number)
-#number = number // 10
-#third_number = number % 10
-#number = number // 10
-#fourth_number = number % 10
-number = number // 1
-fifth_number = number % 10
-print(fifth_number)
+print(first_number)
+
 
 
